057b73fafcc099d06cc60d28b0f3c47b04317726
DikaRamdani <71133580+DikaRamdani@users.noreply.github.com>
2020-09-11 19:43:59 +0700

    Add files via upload

---
+++ UTSssept.java
@@ -0,0 +1,10 @@
+class UTSsept {
+  
+  public static void main (string [] args) {
+    
+    String str = new string ('A');
+    char ch = str.charAt (1);
+      System.out.println (ch) ;
+  }
+ 
+}
