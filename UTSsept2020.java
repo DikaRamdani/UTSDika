95fe89afd141f17b102f5b61c972267c0ac51c13
DikaRamdani <71133580+DikaRamdani@users.noreply.github.com>
2020-09-11 20:03:28 +0700

    Add files via upload

---
+++ UTSsept2020.java
@@ -0,0 +1,11 @@
+
+ class UTSsept {
+  public static void main (String[] args) {
+    
+  String[] huruf = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"," "};
+  System.out.println(huruf[3]+huruf[8]+huruf[10]+huruf[0]+" "+huruf[17]+huruf[0]+huruf[12]+huruf[3]+huruf[0]+huruf[13]+huruf[8]);
+  
+  }
+  
+ }
+ 
